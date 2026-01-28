be74d702249d62faf99b402c3297a05ec1cd9f3b
lekuan <e1366703@u.nus.edu>
2026-01-28 13:58:37 +0800

    Add Level-2: Add, List
    Implement task storage and listing:
    - Store tasks in String array (max 100)
    - Add tasks with any input except 'list' and 'bye'
    - List all tasks with 'list' command
    - Format output with horizontal lines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,18 +13,32 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+            
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + "." + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
         }
     }
 }
