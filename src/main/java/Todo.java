10e40c44a2496bbec9a8617345f124605bc2a4d3
lekuan <e1366703@u.nus.edu>
2026-02-09 18:23:12 +0800

    Add A-CodingStandard: following the coding standards

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,5 +1,13 @@
+/**
+ * Represents a todo task without any date constraints.
+ */
 public class Todo extends Task {
 
+    /**
+     * Creates a new Todo task with the given description.
+     *
+     * @param description The description of the todo.
+     */
     public Todo(String description) {
         super(description);
     }
